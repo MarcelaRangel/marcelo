82199fceb403388dbb5f60b3232c3cc5e329e6c6
MarcelaRangel <52613221+MarcelaRangel@users.noreply.github.com>
2020-02-08 18:49:38 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+ <div class="container"> 
+  <div class="col s10">
+		<div class="col s5 black">
+			<div class="container">
+			<h5 class = "noselect white-text center-align">© Marcela Todos os Direitos Reservados.</h5>
+			</div>
+		</div>
+  </div>
+ </div>
+		<!-- declaração do javascript -->
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+  </html>
---
+++ topo.php
@@ -0,0 +1,9 @@
+ <div class="container"> 
+  <div class="col s5">
+	<nav class="black">
+		<div class="col s5">
+			<a href="#" class="brand-logo center">Controle de Produtos</a>
+		</div>
+	</nav>
+  </div>
+</div>
