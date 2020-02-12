194f59283bf88345679083a78cbf6cc20cd5805e
MarcelaRangel <52613221+MarcelaRangel@users.noreply.github.com>
2020-02-12 20:04:58 -0300

    Update new 1.php

--- new 1.php
+++ new 1.php
@@ -1,9 +1,13 @@
 
+<?php
+session_start();
+?>
+
 <html>
     <head>
 	  <title>Controle de Produtos</title>
       <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 	  <link rel="stylesheet" href="css/materialize" />
@@ -14,6 +18,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 <body>
+<div  align="center"class="container">
  <div class="container"> 
   <div class="col s5">
 	<nav class="black">
@@ -22,51 +27,50 @@
 		</div>
 	</nav>
   </div>
-</div><div class="container">
-	<div class="row">
-		<br>
-		<div class="col s2 center">
-		<a href="addproduto.php" <i class="btn-floating btn-large waves-effect waves-light cian"> <i class="material-icons">add</i> </a> 
-		<h6>Adicionar Produto</h6>
-		</div>
-		<div class="col s2 center">
-		<a class="btn-floating btn-large waves-effect waves-light cian"><i class="material-icons">delete_forever</i></a>
-		<h6>Excluir<br>Produto</h6>
-		</div>
-		<div class="col s2 center">
-		<a class="btn-floating btn-large waves-effect waves-light cian"><i class="material-icons">find_in_page</i></a>
-		<h6>Buscar Produto</h6>
-		</div>
-		<div class="col s2 center">
-		<a class="btn-floating btn-large waves-effect waves-light cian"><i class="material-icons">edit</i></a>
-		<h6>Editar Produto</h6>
-		</div>
-		<div class="col s2 center">
-		<a class="btn-floating btn-large waves-effect waves-light cian"><i class="material-icons">exposure_plus_1</i></a>
-		<h6>Adicionar qtde. Produto</h6>
-		</div>
-		<div class="col s2 center">
-		<a class="btn-floating btn-large waves-effect waves-light cian"><i class="material-icons">remove</i></a>
-		<h6>Retirar qtde. Produto</h6>
-		</div>
-	</div>
-  </div>
 </div>
-  
+    <div class="container"> 
 
- <div class="container"> 
-  <div class="col s10">
+
+	<div   class="row " >
+		<br>
+	
+		<div class="col s2 center"" >
+		<a href="addprodt.php" <i class="btn-floating btn-large waves-effect waves-light cian"> <i class="material-icons">add</i> </a> 
+		<h6>Cadastrar<br>Prodruto</h6>
+		</div>
+		<div href="estoque.php" class="col s8 center">
+		<a class="btn-floating btn-large waves-effect waves-light cian"><i class="material-icons">find_in_page</i></a>
+		<h6>Estoque</h6>
+		</div>
+		<div class="col s2 center">
+		<a class="btn-floating btn-large waves-effect waves-light cian"><i class="material-icons">add_shopping_cart</i></a>
+		<h6>Nova Venda</h6>
+		</div>
+</div>
+	 </div>
+  
+   <div class="container"> 
+  
 		<div class="col s5 black">
 			<div class="container">
-			<h5 class = "noselect white-text center-align">Â© Marcela Todos os Direitos Reservados.</h5>
+			
+			<h6 class = "noselect white-text center-align">© Marcela Rangel Todos os Direitos Reservados.</h6>
 			</div>
 		</div>
   </div>
+ 
+
+  
+
+ 
+ 
  </div>
 		<!-- declaraÃ§Ã£o do javascript -->
       <script type="text/javascript" src="js/materialize.min.js"></script>
     </body>
+
   </html>
-<?php
-include 'resources/footer.php';
-?>
+
+  </html>
+
+
